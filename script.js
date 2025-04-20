ddfa072dc2bb150590d416986781395540828b5d
amuseDave <amusedave2@gmail.com>
2025-04-20 15:19:05 +0300

    Implement brush softness and random color checkbox

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
-import { drawLine, getCanvasSizeCSSRatio, initiateCanvasSettings } from "./utils.js";
+import {
+  drawLine,
+  getCanvasSizeCSSRatio,
+  getRandomColor,
+  initiateCanvasSettings,
+} from "./utils.js";
 
 const canvasEl = document.getElementById("canvas");
 const ctx = canvasEl.getContext("2d");
 
 const canvasSettingsEl = document.getElementById("canvas-settings");
 const clearCanvasBtn = document.getElementById("clear-canvas");
+const checkboxEl = document.getElementById("checkbox");
+const checkboxInputEl = document.getElementById("checkbox-input");
+
+const softnessEl = document.getElementById("softness");
+const softnessInputEl = document.getElementById("softness-input");
 
 initiateCanvasSettings(ctx);
 
-const state = { isClicked: false, ...getCanvasSizeCSSRatio(canvasEl) };
+const state = { isClicked: false, ...getCanvasSizeCSSRatio(canvasEl), isRandom: false };
 
 function handleMouseUp() {
   state.isClicked = false;
@@ -20,6 +30,7 @@ function handleMouseDown(e) {
 
   state.isClicked = true;
 
+  if (state.isRandom) ctx.strokeStyle = getRandomColor();
   ctx.beginPath();
   ctx.moveTo(x, y);
   drawLine(x, y, ctx);
@@ -27,6 +38,7 @@ function handleMouseDown(e) {
 
 function handleMouseMove(e) {
   if (!state.isClicked) return;
+  if (state.isRandom) ctx.strokeStyle = getRandomColor();
   const { x, y } = { x: e.offsetX * state.width, y: e.offsetY * state.height };
   drawLine(x, y, ctx);
 }
@@ -38,17 +50,35 @@ canvasEl.addEventListener("mousemove", handleMouseMove);
 
 // Canvas settings
 
-canvasSettingsEl.addEventListener("input", (e) => {
-  const { value, type } = e.target;
+canvasSettingsEl.addEventListener("change", (e) => {
+  const { value, type, id } = e.target;
   if (type === "range") {
     ctx.lineWidth = typeof +value === "number" ? +value : ctx.lineWidth;
   } else if (type === "color") {
     ctx.strokeStyle = value;
-  } else if (type === "submit") {
-    console.log(type);
+  } else if (type === "checkbox") {
+    if (id === "checkboxx") {
+      checkboxInputEl.checked = !checkboxInputEl.checked;
+      state.isRandom = checkboxInputEl.checked;
+    } else {
+      softnessInputEl.checked = !softnessInputEl.checked;
+      state.isRandom = softnessInputEl.checked;
+      ctx.globalAlpha = softnessInputEl.checked ? 0.01 : 1;
+    }
   }
 });
 
 clearCanvasBtn.addEventListener("click", () => {
-  ctx.clearRect(0, 0, 1000, 600);
+  ctx.clearRect(0, 0, 1000, 800);
+});
+
+checkboxEl.addEventListener("click", () => {
+  checkboxInputEl.checked = !checkboxInputEl.checked;
+  state.isRandom = checkboxInputEl.checked;
+});
+
+softnessEl.addEventListener("click", () => {
+  softnessInputEl.checked = !softnessInputEl.checked;
+  state.isRandom = softnessInputEl.checked;
+  ctx.globalAlpha = softnessInputEl.checked ? 0.01 : 1;
 });
--- utils.js
+++ utils.js
@@ -14,7 +14,28 @@ export function getCanvasSizeCSSRatio(canvasEl) {
 }
 
 export function initiateCanvasSettings(ctx) {
-  ctx.lineWidth = 15;
+  ctx.lineWidth = 10;
   ctx.lineJoin = "round";
   ctx.lineCap = "round";
+}
+
+const colors = [
+  "#A3C9F1",
+  "#FF6B8E",
+  "#4AD8C2",
+  "#EFAF33",
+  "#7B4CE8",
+  "#D192B6",
+  "#3FC75A",
+  "#F943D0",
+  "#86E39A",
+  "#C81E4F",
+  "#5AB7F0",
+  "#FF9C64",
+  "#2E8BD4",
+  "#B46CF8",
+  "#E6D52A",
+];
+export function getRandomColor() {
+  return colors[Math.floor(Math.random() * colors.length)];
 }
